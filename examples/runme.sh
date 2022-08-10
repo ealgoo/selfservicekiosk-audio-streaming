8e18b67905f7f800b5ebaac4fd729adda4dbeb2e
Minjeong Kim <minjeongsunnykim@gmail.com>
2022-08-10 08:07:16 +0000

    connecting to sql

--- examples/runme.sh
+++ examples/runme.sh
@@ -1,6 +1,5 @@
-cd /home/ealgoo/selfservicekiosk-audio-streaming/examples
+cd /home/minjeongsunnykim/selfservicekiosk-audio-streaming/examples
 PROJECT_ID=yeggi-344007
 gcloud config set project $PROJECT_ID
-export GOOGLE_APPLICATION_CREDENTIALS="/home/ealgoo/selfservicekiosk-audio-streaming/examples/yeggi-344007-31d7ab182845.json"
+export GOOGLE_APPLICATION_CREDENTIALS="/home/minjeongsunnykim/selfservicekiosk-audio-streaming/examples/yeggi-344007-31d7ab182845.json"
 npm --example=yeggi --port=8080 --project_id=$PROJECT_ID run start
-
